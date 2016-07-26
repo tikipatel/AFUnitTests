cb441f89645a85f04e475c020de06ec898fb4e6c
Pratikbhai Patel <pratikpatel@me.com>
2016-07-26 09:46:25 -0400

    Add Pods and AlamoFire

---
+++ Pods/Target Support Files/Pods-UnitTest-AlamoFire/Pods-UnitTest-AlamoFire-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_UnitTest_AlamoFireVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_UnitTest_AlamoFireVersionString[];
+
---
+++ Pods/Target Support Files/Pods-UnitTest-AlamoFireTests/Pods-UnitTest-AlamoFireTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_UnitTest_AlamoFireTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_UnitTest_AlamoFireTestsVersionString[];
+
